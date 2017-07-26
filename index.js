f97fba44890e057b09a073228bcd5f88869281fd
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-07-26 19:40:58 -0400

    changed checkbox to submit button and installed body-parser

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
+// ===========PACKAGES===========
+
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
+const bodyParser = require('body-parser')
+
+// =========BOILER PLATE===========
 
 app.engine ('mustache', mustacheExpress());
 app.set ('views', './views');
 app.set ('view engine', 'mustache');
 
-app.use (express.static('public'));
+app.use (bodyParser());
 
-const todos = [
-  "Wash the car"
+// =============ARRAYS===============
+
+const todoArray = [
+  { id: 1, text: 'Groceries' },
+  { id: 2, text: 'Dishes'}
+];
+const doneArray = [
+  { id: 3, text: 'Completed'}
 ];
 
-app.get("/", function (req, res) {
-  res.render('bloop', {todos:todos});
-});
+last_id = 3;
 
+// ============HOME PAGE===========
+
+app.get("/", function (req, res) {
+  res.render('bloop',
+  {todos:todoArray, done: doneArray}
+  );
+});
 app.post("/", function (req, res) {
-  todos.push(req.body.todo);
+  last_id += 1;
+  todoArray.push({id: last_id, text: req.body.item });
+
   res.redirect('/');
 });
+
+// =============SUBMIT PAGE===========
+
+app.post("/:id", function(req, res) {
+  let id = req.params.id;
+  // let todos = req.body;
+  // todoArray.push(req.body.todos);
+  res.send(`You submitted ${id}!`);
+});
+
+
 
 app.listen(3000, function() {
   console.log('You started the application!');
